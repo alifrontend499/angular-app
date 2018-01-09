68f3154943f27a012a32b9b65045d3b511e6127f
alifrontend499 <aliofficial499@gmail.com>
2018-01-09 21:34:50 +0530

    changes last

--- login/js/controller/main-controller.js
+++ login/js/controller/main-controller.js
@@ -1,4 +1,8 @@
 app.controller('login', function ($scope) {
   $scope.name = ''
   $scope.password = ''
+  $scope.newName = ''
+  $scope.$watch('name', function (oldValue, newValue) {
+    $scope.newName = newValue
+  });
 })
