75d64319279a4381d21f93c7ee223fa291248edb
alifrontend499 <aliofficial499@gmail.com>
2018-01-12 20:36:39 +0530

    my changes

--- js/controller/mainController.js
+++ js/controller/mainController.js
@@ -34,3 +34,36 @@ app.controller('timeoutFun', function ($scope, $timeout) {
     $scope.name = 'Karan'
   }, 4000)
 });
+// http data controller
+app.controller('httpData', function ($scope, $http) {
+  $http.get('http://localhost/angular-app/json/data.json').then(function (response) {
+    // $scope.maData = [];
+    $scope.maData = response.data;
+  }, function (response) {
+    $scope.maData = response.statusText;
+  });
+});
+// select options with angular
+app.controller('selOpts', function ($scope) {
+  $scope.names = [
+    {
+      name: 'Ali',
+      age: 21,
+      profession: 'Developer'
+    },
+    {
+      name: 'Jaskaran',
+      age: 23,
+      profession: 'Developer'
+    },
+    {
+      name: 'Jaswinder',
+      age: 28,
+      profession: 'Developer (beginner)'
+    }
+  ];
+});
+// routing practice
+app.controller('rCont', ['$scope', '$log', '$location', function ($scope, $log, $location) {
+  $log.info($location.path())
+}]);
